f19eebe96621c34194aa3434453c44acc795d247
Marc985 <hei.marc.3@gmail.com>
2024-08-04 16:28:44 +0300

    chore:write csv file

--- transform/transform.py
+++ transform/transform.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 def clean_and_transform_data(input_filename='pollution_data.csv', output_filename='transformed_pollution_data.csv'):
     df = pd.read_csv(input_filename)
 
@@ -14,12 +15,10 @@ def clean_and_transform_data(input_filename='pollution_data.csv', output_filenam
         'co': -1
     }, inplace=True)
 
-    #df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
+    df['date'] = pd.to_datetime('today').normalize()
 
-    #df['normalized_aqi'] = df['aqi'] / 300  
-
-   # df['pm2_5_level'] = pd.cut(df['pm2_5'], bins=[0, 12, 35, 55, np.inf], labels=['Good', 'Moderate', 'Unhealthy', 'Very Unhealthy'])
-    #df['pm10_level'] = pd.cut(df['pm10'], bins=[0, 50, 150, 250, np.inf], labels=['Good', 'Moderate', 'Unhealthy', 'Very Unhealthy'])
+    df['pm2_5_level'] = pd.cut(df['pm2_5'], bins=[0, 12, 35, 55, np.inf], labels=['Good', 'Moderate', 'Unhealthy', 'Very Unhealthy'])
+    df['pm10_level'] = pd.cut(df['pm10'], bins=[0, 50, 150, 250, np.inf], labels=['Good', 'Moderate', 'Unhealthy', 'Very Unhealthy'])
 
     def get_pollution_level(aqi):
         if aqi <= 50:
@@ -31,16 +30,9 @@ def clean_and_transform_data(input_filename='pollution_data.csv', output_filenam
         else:
             return 'Very Unhealthy'
 
-   # df['pollution_level'] = df['aqi'].apply(get_pollution_level)
+    df['pollution_level'] = df['aqi'].apply(get_pollution_level)
 
-
-    mean_values = df.groupby('location').mean().reset_index()
-    
-    mean_values.to_csv(output_filename, index=False)
-    print(mean_values)
-
-
-    #df.to_csv(output_filename, index=False)
-    return mean_values
+    df.to_csv(output_filename, index=False)
+    return df
 
 
